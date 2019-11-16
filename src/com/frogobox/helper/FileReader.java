630b3a6b081f28b9ac257df4301a5e9c8cb07f0b
amirisback <faisalamircs@gmail.com>
2019-11-16 16:19:36 +0700

    add: FileReader.java

---
+++ src/com/frogobox/helper/FileReader.java
@@ -0,0 +1,20 @@
+package com.frogobox.helper;
+
+/**
+ * Created by Faisal Amir
+ * FrogoBox Inc License
+ * =========================================
+ * GeneticAlgorithm
+ * Copyright (C) 16/11/2019.
+ * All rights reserved
+ * -----------------------------------------
+ * Name     : Muhammad Faisal Amir
+ * E-mail   : faisalamircs@gmail.com
+ * Github   : github.com/amirisback
+ * LinkedIn : linkedin.com/in/faisalamircs
+ * -----------------------------------------
+ * FrogoBox Software Industries
+ * com.frogobox.helper
+ */
+public class FileReader {
+}
